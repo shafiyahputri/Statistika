5f30491b0bd8d59eebf49c1b3e272051e2a49d47
Shafiyah Putri <112670948+shafiyahputri@users.noreply.github.com>
2022-12-12 13:30:11 +0700

    Add files via upload

---
+++ TA Distribusi Multinomial.R
@@ -0,0 +1,19 @@
+# KUISIONER 1 UNEJ ONLY
+#pertanyaan 1
+dmultinom(x=c(3,15,55,27), prob=c(.25, .25, .25, .25))
+
+#pertanyaan 2
+dmultinom(x=c(3,11,41,45), prob=c(.25, .25, .25, .25))
+
+#pertanyaan 3
+dmultinom(x=c(5,24,43,28), prob=c(.25, .25, .25, .25))
+
+# KUISIONER 2 UMUM
+#pertanyaan 1
+dmultinom(x=c(5,27,48,20), prob=c(.25, .25, .25, .25))
+
+#pertanyaan 2
+dmultinom(x=c(2,5,40,53), prob=c(.25, .25, .25, .25))
+
+#pertanyaan 3
+dmultinom(x=c(5,20,43,32), prob=c(.25, .25, .25, .25))
