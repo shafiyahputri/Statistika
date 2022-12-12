533cdd9442b915b21c15ac1b3cf49c5f4300c5af
Shafiyah Putri <112670948+shafiyahputri@users.noreply.github.com>
2022-12-12 19:56:48 +0700

    Add files via upload

---
+++ TA Distribusi Multinomial.R
@@ -0,0 +1,17 @@
+# KUISIONER 1 9 Des
+#probabilitas 1 
+dmultinom(x=c(3,3,1,5), prob=c(.25, .25, .25, .25))
+
+#probabilitas 2 
+dmultinom(x=c(3,3,2,4), prob=c(.25, .25, .25, .25))
+
+#probabilitas 3 
+dmultinom(x=c(3,3,3,3), prob=c(.25, .25, .25, .25))
+
+#KUISIONER 2 10 Des
+#probabilitas 1 
+dmultinom(x=c(2,2,1,3), prob=c(.25, .25, .25, .25))
+
+#probabilitas 2 
+dmultinom(x=c(2,2,2,2), prob=c(.25, .25, .25, .25))
+
